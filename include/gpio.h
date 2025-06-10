9fa3516f0cab0994685a3bbd944533f4af369dd7
AJQuattropani <ajenningsquattrop@unm.edu>
2025-06-10 13:43:41 -0600

    Moved dummy read to hardware.h, since it is useful across many different places.

--- include/gpio.h
+++ include/gpio.h
@@ -15,15 +15,9 @@
 #define GP_ALT 0b10
 #define GP_ANLG 0b11
 
+
 #define ENABLE_GPIO_A RCC->AHB1ENR |= (1 << RCC_AHB1ENR_GPIOAEN_Pos)
 #define ENABLE_GPIO_B RCC->AHB1ENR |= (1 << RCC_AHB1ENR_GPIOBEN_Pos)
-
-#define DUMMY_READ_FROM(x) \
-{ \
-  volatile uint32_t __dummy__r; \
-  __dummy__r = x; \
-  __dummy__r = x; \
-}
 
 #define GP_REG_SET(reg, val, pos) reg |= (val << pos)
 #define GP_REG_TGB(reg, pos) reg ^= (1 << pos)
